2a53fb2b74792b6623c67c215fde6adc879bb2d3
fxdx <pub.myself666@gmail.com>
2022-05-17 18:13:22 +0100

    improved command line parsing and changed project name

--- wcec_dalvik/bin/main.ml
+++ wcec_dalvik/bin/main.ml
@@ -1,41 +1,37 @@
 open Analysis
 open Lexparse
 
-let cfg_method_number p = 
-  match p with 
-  |Cfg.Empty_cfg -> 0
-  |Cfg.Icfg(cfg)->Hashtbl.length cfg.cfgs
 
-let multiple_returns_count p = 
-  match p with 
-  |Cfg.Empty_cfg -> 0
-  |Cfg.Icfg(cfg)->
-    let i = ref 0 in 
-    Hashtbl.iter (
-      fun _ m -> match m with |Cfg.Empty_method -> ()
-      |Method(m')-> if List.length (m'.exit) > 0  then incr(i);
-    ) cfg.cfgs;
-    !i
+let usage_msg = "wcec [-verbose] <file>"
+let verbose = ref false
+let input_files = ref []
+let speclist =
+  [
+    ("-verbose", Arg.Set verbose, "Output debug information");
+  ]
 
-
-let cfg_info = false
+let anon_fun filename =
+  input_files := filename::!input_files
 
 let () = 
-  let file = Sys.argv.(1) in 
-  let c = open_in file in 
-  Printf.printf "Input file: %s\n" file;
-  let lb = Lexing.from_channel c in 
-  let p = Loadprog.prog lb in
-  Printf.printf "Lexing and Parsing done\n";
-  let hp = Hashdvk.hash_program p in 
-  Printf.printf "Hashing done\n";
-  let h_m_number = Hash2cfg.methods_number hp in
-  Printf.printf "Hashed methods number: %i\n" h_m_number;
-  let cfg = Hash2cfg.transform_program hp in 
-  Printf.printf "CFG building done\n";
-  if cfg_info then begin
-    let cfg_method_n = cfg_method_number cfg in 
-    Printf.printf "CFG methods number: %i\n" cfg_method_n;
-    let mult_r = multiple_returns_count cfg in 
-    Printf.printf "Multiple returns number: %i\n" mult_r
-  end;
+  Arg.parse speclist anon_fun usage_msg;
+  List.iter (fun file -> 
+    let c = open_in file in 
+    Printf.printf "\nInput file: %s\n" file;
+    let lb = Lexing.from_channel c in 
+    let p = Loadprog.prog lb in
+    Printf.printf "Lexing and Parsing done\n";
+    let hp = Hashdvk.hash_program p in 
+    Printf.printf "Hashing done\n";
+    let h_m_number = Hash2cfg.methods_number hp in
+    if !verbose then 
+    Printf.printf "Hashed methods number: %i\n" h_m_number;
+    let cfg = Hash2cfg.transform_program hp in 
+    Printf.printf "CFG building done\n";
+    if !verbose then begin
+      let cfg_method_n =  Dbg_tools.cfg_method_number cfg in 
+      Printf.printf "CFG methods number: %i\n" cfg_method_n;
+      let mult_r = Dbg_tools.multiple_returns_count cfg in 
+      Printf.printf "Multiple returns number: %i\n" mult_r
+    end;
+    print_newline ();) (List.rev !input_files)
