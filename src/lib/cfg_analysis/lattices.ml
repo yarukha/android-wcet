cd7e559aa5d307556e52b57ccb8aaa071254f74e
DelcioKelson <delciokelson@outlook.com>
2023-05-16 01:31:21 +0100

    string lattice

--- src/lib/cfg_analysis/lattices.ml
+++ src/lib/cfg_analysis/lattices.ml
@@ -4,11 +4,13 @@ module type AbstractDomain = sig
   
   val top : t 
   
-  val bottom : t 
+  val bot              : t 
   
   val join : t -> t -> t 
   
   val union : t -> t -> t
+
+  (*val compare: t -> t -> int*)
   
 end 
 
@@ -16,21 +18,41 @@ end
 module MakeSimpleLattice (T: sig type t end) : AbstractDomain = 
 struct
 
-  type t = Bottom | Value of T.t | Top
+  type t = Bot | Value of T.t | Top
 
-  let bottom = Bottom
+  let bot = Bot
 
   let top = Top 
 
   let join a b = 
     match a,b with 
-    | Bottom, x | x, Bottom -> x
+    | Bot, x | x, Bot -> x
     | Top, _ | _, Top -> Top 
     | Value x , Value y -> if x = y then Value x else Top 
 
   let union = join
 
+  (*let compare a b =
+      match a, b with
+      | Bottom, Bottom | Top, Top -> 0
+      | Bottom, _ | _, Top -> -1
+      | _, Bottom | Top, _ -> 1
+      | Value x, Value y -> compare x y*)
 end 
+
+module StringSetDomain : AbstractDomain = struct
+  module StringSet = Set.Make(String)
+
+  type t = Bot | StrSet of StringSet.t | Top
+  let bot = Bot
+  let top = Top
+  let join a b = match a, b with
+    | Bot, x | x, Bot -> x
+    | StrSet x, StrSet y -> StrSet (StringSet.union x y)
+    | _, _ -> Top
+  let union = join
+end
+
 
 
 module IntegerLattice : AbstractDomain = MakeSimpleLattice (struct type t = int end)
